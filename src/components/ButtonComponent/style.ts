194a8df0b753c01889102a936cb163ba9f554145
Luis Gustavo Sales Dev <luisgssf@gmail.com>
2022-06-27 11:11:54 -0300

    Colors for all components was fixes.

--- src/components/ButtonComponent/style.ts
+++ src/components/ButtonComponent/style.ts
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 
 export const MenuLink = styled(NavLink)`
   border-radius: 5px;
-  background: var(--green);
+  background: ${ ({ theme }) => theme.colors.green };
   padding: 10px;
-  color: var(--shape);
+  color: ${ ({ theme }) => theme.colors.shape };
   cursor: pointer;
   text-decoration: none ;
 
